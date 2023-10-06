f20422ef8b8de1c954a39d238f474a8da4ad81fa
Mohsen Zainalpour <zainalpour@yahoo.com>
2023-10-05 21:29:16 -0400

    style: Format code to match rustfmt settings

--- src/bin/indexer.rs
+++ src/bin/indexer.rs
@@ -1,26 +1,27 @@
+use std::path::PathBuf;
+
 use anyhow::Result;
 use clap::Parser;
 use env_logger::Env;
 use ircc_ai::fs::list_files_recursively;
-use std::path::PathBuf;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    #[arg(short, long)]
-    path: String,
+	#[arg(short, long)]
+	path: String
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    dotenv::dotenv().ok();
+	dotenv::dotenv().ok();
 
-    env_logger::init_from_env(Env::default().default_filter_or("info"));
+	env_logger::init_from_env(Env::default().default_filter_or("info"));
 
-    let args = Args::parse();
+	let args = Args::parse();
 
-    let dir = PathBuf::from(args.path);
-    list_files_recursively(dir).await?;
+	let dir = PathBuf::from(args.path);
+	list_files_recursively(dir).await?;
 
-    Ok(())
+	Ok(())
 }
--- src/fs/mod.rs
+++ src/fs/mod.rs
@@ -1,32 +1,30 @@
-use crate::prelude::Result;
-use async_recursion::async_recursion;
-
-use log::{debug, info};
 use std::path::PathBuf;
+
+use async_recursion::async_recursion;
+use log::{debug, info};
 use tokio::fs;
 use tokio::fs::File;
 use tokio::io::AsyncReadExt;
 
+use crate::prelude::Result;
+
 #[async_recursion(?Send)]
 pub async fn list_files_recursively(dir: PathBuf) -> Result<()> {
-    let mut entries = fs::read_dir(dir).await?;
+	let mut entries = fs::read_dir(dir).await?;
 
-    while let Some(res) = entries.next_entry().await? {
-        let path = res.path();
-        if path.is_dir() {
-            list_files_recursively(path.clone()).await?;
-        } else {
-            info!("Processing: {}", path.display());
+	while let Some(res) = entries.next_entry().await? {
+		let path = res.path();
+		if path.is_dir() {
+			list_files_recursively(path.clone()).await?;
+		} else {
+			info!("Processing: {}", path.display());
 
-            let mut file = File::open(path).await?;
+			let mut file = File::open(path).await?;
 
-            let mut buffer = String::new();
-            file.read_to_string(&mut buffer).await?;
-            debug!(
-                "Contents: {:?}",
-                buffer.chars().take(10).collect::<Vec<char>>()
-            );
-        }
-    }
-    Ok(())
+			let mut buffer = String::new();
+			file.read_to_string(&mut buffer).await?;
+			debug!("Contents: {:?}", buffer.chars().take(10).collect::<Vec<char>>());
+		}
+	}
+	Ok(())
 }
