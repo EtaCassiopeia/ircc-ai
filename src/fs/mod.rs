e566fdacb15c14735f2b7ffc3a34f427c90ce5f1
Mohsen Zainalpour <zainalpour@yahoo.com>
2023-10-15 22:59:13 -0400

    refactor(fs): Improve file embedding and content fetching

--- src/fs/mod.rs
+++ src/fs/mod.rs
@@ -1,11 +1,12 @@
 use std::path::PathBuf;
 use std::sync::Arc;
 
+use anyhow::Error;
 use async_recursion::async_recursion;
+use futures::stream::StreamExt;
 use log::{debug, info};
-use tokio::fs::File;
-use tokio::io::AsyncReadExt;
-use tokio::{fs, task};
+use tokio::fs;
+use tokio::time::Duration;
 
 use crate::{
 	embeddings::{Embeddings, EmbeddingsModel},
@@ -18,7 +19,7 @@ pub struct FileEmbeddings {
 	pub embeddings: Embeddings
 }
 
-pub async fn list_files_recursively(dir: PathBuf) -> Result<Vec<PathBuf>> {
+async fn list_files_recursively(dir: PathBuf) -> Result<Vec<PathBuf>> {
 	#[async_recursion(?Send)]
 	async fn helper(dir: PathBuf, files: &mut Vec<PathBuf>) -> Result<()> {
 		info!("Processing: {}", dir.display());
@@ -43,32 +44,39 @@ pub async fn list_files_recursively(dir: PathBuf) -> Result<Vec<PathBuf>> {
 pub async fn embed_path<M: EmbeddingsModel + Send + Sync + 'static>(model: Arc<M>, path: PathBuf) -> Result<Vec<FileEmbeddings>> {
 	let files = list_files_recursively(path).await?;
 
-	let embedding_futures: Vec<_> = files
-		.into_iter()
-		.map(|path| {
-			let model_clone = Arc::clone(&model);
-			task::spawn(async move {
-				let file_content = fs::read_to_string(path.clone()).await.unwrap();
-				let embeddings = model_clone.embed(&file_content).unwrap();
-				FileEmbeddings {
-					path: path.to_str().unwrap().to_string(),
-					embeddings
-				}
+	let file_embeddings_stream = futures::stream::iter(files.into_iter()).then(|path| {
+		let model_clone = Arc::clone(&model);
+		async move {
+			let file_content = fetch_file_content(path.clone()).await?;
+			let embeddings = model_clone.embed(&file_content)?;
+			log::info!("Embeddings for {} calculated", path.display());
+			Ok(FileEmbeddings {
+				path: path.to_str().unwrap().to_string(),
+				embeddings
 			})
-		})
-		.collect();
+		}
+	});
 
-	let file_embeddings: Vec<_> = futures::future::join_all(embedding_futures).await.into_iter()
-        .map(|res| res.unwrap())  // unwrap the Result returned by task::spawn
-        .collect();
+	let file_embeddings_results: Vec<Result<FileEmbeddings>> = file_embeddings_stream.collect().await;
 
-	Ok(file_embeddings)
+	let file_embeddings: Result<Vec<FileEmbeddings>> = file_embeddings_results.into_iter().collect();
+
+	log::info!("Calculated embeddings for {} files", file_embeddings.as_ref().map(Vec::len).unwrap_or(0));
+
+	file_embeddings
 }
 
-pub async fn fetch_file_content(path: &str) -> Result<String> {
-	let mut file = File::open(path).await?;
+async fn fetch_file_content(path: PathBuf) -> Result<String> {
+	let timeout = Duration::from_secs(60); // Adjust the timeout as needed.
+	let result = tokio::time::timeout(timeout, async {
+		let file_content = fs::read_to_string(path).await?;
+		anyhow::Ok(file_content)
+	})
+	.await;
 
-	let mut buffer = String::new();
-	file.read_to_string(&mut buffer).await?;
-	Ok(buffer)
+	match result {
+		Ok(Ok(content)) => Ok(content),
+		Ok(Err(err)) => Err(err.into()),
+		Err(_) => Err(Error::msg("File content fetching timed out."))
+	}
 }
